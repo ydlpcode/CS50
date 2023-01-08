823727fa6d0d32d8ea0dbc2692002d5399f02790
Eduardo Gonçalves Chambingo <108438916+ydlpcode@users.noreply.github.com>
2023-01-08 14:04:25 +0100

    Create hello_user.py

---
+++ introduction_to_programming_with_python/lecture_2/hello_user.py
@@ -0,0 +1,8 @@
+# pergunta ao usuário pelo seu nome
+nome = input("Qual o teu nome? ")
+
+# diz olá ao usuário + o seu nome usando concatenação
+print("Hello, " + nome) 
+
+# diz olá ao usuário + o seu nome usando dois argumentos
+print("Hello", nome) 
